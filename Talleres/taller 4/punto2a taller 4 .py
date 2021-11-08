d653f23f7e76d73d8ea18c1343f690d47368c595
MrZ0mbi3 <38089758+MrZ0mbi3@users.noreply.github.com>
2021-11-07 21:48:08 -0500

    Add files via upload

--- Talleres/taller 4/punto2a taller 4 .py
+++ Talleres/taller 4/punto2a taller 4 .py
@@ -7,28 +7,56 @@ This is a temporary script file.
 import math
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import style
+style.use('ggplot')
 
 tx=[]
 ty=[]
+tyExacta=[]
+error=[]
+
+def funcionExacta(t):
+    return ((1/43)*t*math.e**(3*t))-((math.e**(3*t))/1849)
 
 def funcion(t,y):
-    return (t**(3*t))-40*y
+    return (t*math.e**(3*t))-40*y
 
 def metodoEuler(a,b,y0,n):
-    
-    h=(b-a)/n
+    tx.append(a)
+    ty.append(y0)
     t0=a
-    i=0
-    while i< n:
-        ty.append(i)
-        y1=y0+(h*funcion(t0,y0))
-        t0=a+i*h
-        y0=y1
-        print("(",t0,",",y0,")")
+    tyExacta.append(funcionExacta(t0))
+    print("exacta ","(",t0,",",funcionExacta(t0),")")
+    print("euler ","(",t0,",",y0,")")
+    h=(b-a)/n
+    
+    
+    for i in range(n):
+        tyExacta.append(funcionExacta(t0))
+        print("exacta ","(",t0,",",funcionExacta(t0),")")
+        y0=y0 + h*funcion(t0,y0) 
+        error.append(abs((funcionExacta(t0)-y0)/funcionExacta(t0)))
+        ty.append(y0)
+        t0=t0+h
         tx.append(t0)
-        i=i+0.1
+        
+        print("euler ","(",t0,",",y0,")")
+    
     
 
-metodoEuler(1, 2, 10, 10)
+metodoEuler(0, 2, 10, 100)
 plt.plot(tx,ty)
 plt.show()
+
+plt.plot(tx, tyExacta)
+plt.show()
+
+suma=0
+print(error)
+
+for i in range(3,len(error)):
+    
+    suma=suma+error[i]
+
+errorPromedio=suma/len(error)
+print(errorPromedio)
